815947dba5e35f12921408d7a045188643011dfb
Alfred3002 <orapaalfred19@gmail.com>
2025-07-31 16:57:44 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const message = ref('Task')
+const newSecurity = ref("")
+
+function formSubmitted() {
+  console.log(newSecurity.value)
+}
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+    <form @submit.prevent="formSubmitted">
+      <label>
+        New task
+        <input v-model="newSecurity" name="newTask" />
+      </label>
+      <div class="button-container">
+        <button>Add</button>
+      </div>
+    </form>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
